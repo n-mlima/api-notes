2c60b54735a41317bdb9f0a38f606786b9df3ac0
Nailson Lima <147450945+n-mlima@users.noreply.github.com>
2024-01-09 02:36:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,18 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-
+const corsOptions = {
+  origin: 'https://notes-nlima.netlify.app/', // Substitua pelo domínio do seu frontend
+  optionsSuccessStatus: 200, // alguns navegadores requerem esse código de status
+};
 
 const PORT = process.env.PORT || 5000;
 
-
-// Middleware para permitir solicitações de origens diferentes (CORS)
-app.use(cors());
-// Middleware para analisar o corpo das solicitações como JSON
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
-// Dados de exemplo
-let listdo = [
-  
-];
+let listdo = []; 
+
 
 // Rota para obter todas as tarefas
 app.get('/listdo', (req, res) => {
