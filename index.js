af4522ea0114e5e853ec13491e50f7fb0dcc62f0
Nailson Lima <147450945+n-mlima@users.noreply.github.com>
2024-01-09 00:22:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
-  res.setHeader('Content-Security-Policy', 'font-src \'self\' *.vercel.com *.gstatic.com');
+  res.setHeader('Content-Security-Policy', 'font-src \'self\' *.vercel.com *.gstatic.com *.netlify.com');
   next();
 });
 
