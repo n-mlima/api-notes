930a52f62c89ad98fe58e9e138b2c7c9ad03a9a2
Nailson Lima <147450945+n-mlima@users.noreply.github.com>
2024-01-08 22:18:36 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = 5000;
+
+// Middleware para permitir solicitações de origens diferentes (CORS)
+app.use(cors());
+// Middleware para analisar o corpo das solicitações como JSON
+app.use(bodyParser.json());
+
+// Dados de exemplo
+let listDo = [
+  
+];
+
+// Rota para obter todas as tarefas
+app.get('/listdo', (req, res) => {
+  res.json(listDo);
+});
+
+// Rota para criar uma nova tarefa
+app.post('/listdo', (req, res) => {
+  const newTask = req.body;
+  newTask.id = Math.floor(Math.random() * (10000 - 1) + 1);
+  listDo.push(newTask);
+  res.json(newTask);
+});
+
+// Rota para atualizar uma tarefa
+app.put('/listdo/:id', (req, res) => {
+  const taskId = parseInt(req.params.id);
+  const updatedTask = req.body;
+
+  listDo = listDo.map((task) =>
+    task.id === taskId ? { ...task, ...updatedTask } : task
+  );
+
+  res.json(updatedTask);
+});
+
+// Rota para excluir uma tarefa
+app.delete('/listdo/:id', (req, res) => {
+  const taskId = parseInt(req.params.id);
+  listDo = listDo.filter((task) => task.id !== taskId);
+  res.json({ message: 'Task deleted successfully' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
